a5e6eeb068a3736f97454c987c21a2e5af3c5726
Shreshth Srivastav <44840043+ShreshthSrivastav@users.noreply.github.com>
2023-01-11 21:58:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,18 +42,16 @@ def load_image(image_file):
 	return img
 
 if choice == "Image":
-		st.subheader("Image")
-		image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
+  st.subheader("Image")
+image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
 
-		if image_file is not None:
+if image_file is not None:
+  # To See details
+  file_details = {"filename":image_file.name, "filetype":image_file.type,"filesize":image_file.size}
+  st.write(file_details)
 
-			  # To See details
-			  file_details = {"filename":image_file.name, "filetype":image_file.type,
-                              "filesize":image_file.size}
-			  st.write(file_details)
-
-              # To View Uploaded Image
-			  st.image(load_image(image_file),width=250)
+# To View Uploaded Image
+  st.image(load_image(image_file),width=250)
 
 
 
