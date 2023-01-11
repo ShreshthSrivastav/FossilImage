2c298d31cd9aee2b18cc83fb751e4ff546f2516b
Shreshth Srivastav <44840043+ShreshthSrivastav@users.noreply.github.com>
2023-01-11 20:40:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,21 @@ import pandas as pd
 # App title
 st.title("Fossil Image Classifier")
 
+def main():
+	st.title("File Upload Tutorial")
+
+	menu = ["Image","Dataset","DocumentFiles","About"]
+	choice = st.sidebar.selectbox("Menu",menu)
+
+	if choice == "Image":
+		st.subheader("Image")
+
+    elif choice == "Dataset":
+		st.subheader("Dataset")
+
+    elif choice == "DocumentFiles":
+		st.subheader("DocumentFiles")
+        
 def fetch_image(url):
     response = requests.get(url)
     img = open_image(BytesIO(response.content))
