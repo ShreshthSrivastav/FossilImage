50f34fc00ee0fa3f793fba355a33c58e12a1302c
Shreshth Srivastav <44840043+ShreshthSrivastav@users.noreply.github.com>
2023-01-11 21:56:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,20 +22,44 @@ st.title("Fossil Image Classifier")
 
 # adding a file uploader
 st.title("Upload your own image")
-file = st.file_uploader("Please choose a file")
+# file = st.file_uploader("Please choose a file")
 
-menu = ["Image","Dataset","DocumentFiles","About"]
-choice = st.sidebar.selectbox("Menu",menu)
+# menu = ["Image","Dataset","DocumentFiles","About"]
+# choice = st.sidebar.selectbox("Menu",menu)
+
+# if choice == "Image":
+#   st.subheader("Image")
+
+# elif choice == "Dataset":
+#   st.subheader("Dataset")
+
+# elif choice == "DocumentFiles":
+#   st.subheader("DocumentFiles")
+  
+
+def load_image(image_file):
+	img = Image.open(image_file)
+	return img
 
 if choice == "Image":
-  st.subheader("Image")
+		st.subheader("Image")
+		image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
 
-elif choice == "Dataset":
-  st.subheader("Dataset")
+		if image_file is not None:
 
-elif choice == "DocumentFiles":
-  st.subheader("DocumentFiles")
-        
+			  # To See details
+			  file_details = {"filename":image_file.name, "filetype":image_file.type,
+                              "filesize":image_file.size}
+			  st.write(file_details)
+
+              # To View Uploaded Image
+			  st.image(load_image(image_file),width=250)
+
+
+
+
+
+
 def fetch_image(url):
     response = requests.get(url)
     img = open_image(BytesIO(response.content))
